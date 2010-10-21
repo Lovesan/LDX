f00b0004c6676765518604186076cd21367a5964
Dmitriy Ignatiev <lovesan.ru@gmail.com>
2010-10-21 11:30:31 +0400

    Moved Windows stuff into new library. Check out for Doors: http://github.com/Lovesan/doors

--- com/package.lisp
+++
@@ -1,5 +0,0 @@
-(in-package #:cl-user)
-
-(defpackage #:ldx.com
-  (:use #:cl #:alexandria #:trivial-garbage #:virgil #:ldx.windows)
-  (:export))
--- windows/features.lisp
+++
@@ -1,7 +0,0 @@
-(in-package #:ldx.windows)
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (when (ignore-errors
-          (with-pointer (p "test" '(string :encoding :utf-16le))
-            (deref p '(string :encoding :utf-16le))))
-    (pushnew :ldx.unicode *features*)))
